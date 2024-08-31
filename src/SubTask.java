2708ca7069895d184ea3c639b73b4fdfa2711ef0
Igor TaT <igortat777@yandex.ru>
2024-08-31 16:13:46 +0300

    Разработал подзадачи (класс)

--- src/SubTask.java
+++ src/SubTask.java
@@ -1,2 +1,27 @@
-public class SubTask {
+public class SubTask extends Task {
+    private final int epicID;
+
+
+    public SubTask(String title, String description, int taskId, Status status, int epicID) {
+        super(title, description, taskId, status);
+        this.epicID = epicID;
+    }
+
+    public int getEpicID() {
+        return epicID;
+    }
+
+    @Override
+    public String toString() {
+        return "SubTask{" +
+                "title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", taskId=" + getTaskId() +
+                ", status=" + getStatus() +
+                ", epicID=" + epicID +
+                '}';
+    }
+
+
 }
+
