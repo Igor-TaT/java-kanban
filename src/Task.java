70ccd638cc682f2934252e5481f14ae42686756e
Igor TaT <igortat777@yandex.ru>
2024-08-31 15:44:02 +0300

    Заполнили класс Task методами

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,71 @@
+import java.util.Objects;
+
 public class Task {
+    private String title;
+    private String description;
+    private int taskId;
+    private Status status;
+
+    public Task(String title, String description, int taskId, Status status) {
+        this.title = title;
+        this.description = description;
+        this.taskId = taskId;
+        this.status = status;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return taskId == task.taskId && Objects.equals(title, task.title) && Objects.equals(description, task.description) && status == task.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, taskId, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", taskId=" + taskId +
+                ", status=" + status +
+                '}';
+    }
 }
