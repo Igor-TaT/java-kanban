ac67b60d575d3b8eb02147adefe1a056e7702881
Igor TaT <igortat777@yandex.ru>
2024-08-31 16:29:07 +0300

    Разработал класс для Epic задач

--- src/Epic.java
+++ src/Epic.java
@@ -1,2 +1,34 @@
-public class Epic {
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private ArrayList<SubTask> subTasks;
+
+
+    public Epic(String title, String description, int taskId, Status status) {
+        super(title, description, taskId, status);
+        this.subTasks = new ArrayList<>();
+    }
+
+    public void addSubTask(SubTask subTask) {
+        subTasks.add(subTask);
+    }
+
+    public void cleanAllSubTasks() {
+        subTasks.clear();
+    }
+
+    public ArrayList<SubTask> getSubTasks() {
+        return subTasks;
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", taskId=" + getTaskId() +
+                ", status=" + getStatus() +
+                ", subTasks=" + subTasks +
+                '}';
+    }
 }
