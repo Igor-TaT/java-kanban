bcc9990472e936b1c9ae409e6903c5efae287fff
Igor TaT <igortat777@yandex.ru>
2024-10-05 00:04:37 +0300

    исправили стили, добавили пробел

--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -23,7 +23,7 @@ public class InMemoryTaskManager implements TaskManager {
     }
 
     @Override
-    public List<Task> getHistory(){
+    public List<Task> getHistory() {
         return history.getHistory();
     }
 
